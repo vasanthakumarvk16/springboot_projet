86b947190b76cf84aac04de50854fe4e319b0beb
vasanthakumarvk16 <124858775+vasanthakumarvk16@users.noreply.github.com>
2024-02-21 09:51:52 +0530

    Add files via upload

---
+++ Balancebliss/Balancebliss/src/main/java/com/example/Balancebliss/model/User.java
@@ -0,0 +1,51 @@
+package com.example.Balancebliss.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+    private String userName;
+    private String password;
+    private String emailAddress;
+    
+    public User() {
+    }
+    public User(int userID, String userName, String password, String emailAddress) {
+        this.userID = userID;
+        this.userName = userName;
+        this.password = password;
+        this.emailAddress = emailAddress;
+    }
+    public int getUserID() {
+        return userID;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    
+
+}
---
+++ Balancebliss/Balancebliss/src/test/java/com/example/Balancebliss/BalanceblissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Balancebliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceblissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
