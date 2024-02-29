d198b5854f8c93ddc41ae178810d54ec789e22db
vasanthakumarvk16 <124858775+vasanthakumarvk16@users.noreply.github.com>
2024-02-29 11:17:41 +0530

    Delete Balancebliss/Balancebliss directory

--- Balancebliss/Balancebliss/src/main/java/com/example/Balancebliss/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Balancebliss.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Balancebliss.model.User;
-
-public interface UserRepository extends JpaRepository <User,Integer>{
-    
-}
